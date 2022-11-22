220a393790efe75bf29881ec865c8b2fc984dbcd
unknown <vincius.jesus@sptech.school>
2022-11-22 16:17:53 -0300

    Alterações database

--- site/site/src/database/script-tabelas-agoravita.sql
+++ site/site/src/database/script-tabelas-agoravita.sql
@@ -13,20 +13,7 @@ create table publicacao (
 	id int auto_increment,
     tema varchar(100),
     texto varchar(150),
-    corujas int,
     fkUsuario char(6),
-    foreign key (fkUsuario) references usuario(idUsuario),
+    foreign key (fkUsuario) references usuario(id),
     primary key (idPublicacao, fkUsuario)
-);
-
-create table comentario (
-	fkPublicacao int auto_increment,
-    fkUsuario char(6),
-    comentario varchar(100)
-);
-
-create table engajamento (
-    id int,
-	fkPublicacao int,
-    fkUsuario char(6)
 );
